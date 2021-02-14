2b080d9bb78034256a3f2deef509d01779095c5f
Viktoria <zaharova181114@gmail.com>
2021-02-14 13:38:03 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,40 @@
+// кнопка наверх
+$("body").on("click", ".btn-scroll-up", function (e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: 0
+    }, "slow")
+});
+
+// search dropdown
+$('.btn-search').on('click', function () {
+   $(this).siblings('.search-dropdown').fadeIn();
+});
+
+$(document).mouseup(function (e) { // событие увода мыши
+    let div = $(".search-dropdown"); // тут указываем ID элемента
+    if (!div.is(e.target) // если увели мышку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        div.fadeOut();
+    }
+});
+
+// input search
+$(function () {
+    $('.form-search input').keydown(checkInput).keyup(checkInput);
+});
+
+function checkInput() {
+    if ($('.form-search input').val() === "") {
+        $('.search-result').css('display', 'none');
+    } else {
+        $('.search-result').css('display', 'flex');
+    }
+}
+
+
+// mobile menu
+$('.btn-burger').on('click', function () {
+    $(this).toggleClass('click');
+   $('.mobile-menu').fadeToggle();
+});
