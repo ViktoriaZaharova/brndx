a67ebea6fff7fbfd3d07063b6c2961d904174934
Viktoria <zaharova181114@gmail.com>
2021-03-04 10:11:40 +0200

    add page project page7

--- app/js/common.js
+++ app/js/common.js
@@ -44,7 +44,6 @@ $('.project-row').each(function () {
     if ($(this).find('.project-col').length > 10) {
         $(this).find('.project-col').slice(10).hide();
     }
-
 });
 
 $('.btn-load').on('click', function (e) {
